2b161815d0a330b87a6753fa6f8fdca6abb1b904
Possner Lucas <q619374@europe.bmw.corp>
2024-07-24 14:11:49 +0200

    Implemented NoTearsLinearCausalDiscovery

--- gPC/NoTearsLinearCausalDiscoveryModel.py
+++ gPC/NoTearsLinearCausalDiscoveryModel.py
@@ -1,32 +1,11 @@
+import logging
+from tqdm import tqdm
 import numpy as np
 import networkx as nx
 import pygpc
 from castle.algorithms import Notears
 from castle.datasets import DAG, IIDSimulation
 
-
-# class NoTearsLinearCausalDiscoveryModel(pygpc.AbstractModel):
-
-#     def __init__(self, N_nodes, N_edges, N_samples, method, sem_type):
-#         self.N_nodes = N_nodes
-#         self.N_edges = N_edges
-#         self.N_samples = N_samples
-#         self.method = method
-#         self.sem_type = sem_type
-#         self.model = Notears()
-    
-#     def simulate(self, process_id=None, matlab_engine=None):
-#         W_res_lst = []
-#         for noise_scale in self.p["noise_scale"]:
-#             W_true = DAG.scale_free(n_nodes=self.N_nodes, n_edges=self.N_edges)
-#             data = IIDSimulation(W=W_true, n=self.N_samples, method=self.method, sem_type=self.sem_type, noise_scale=float(noise_scale))
-#             self.model.learn(data.X)
-#             W_res_lst.append((W_true - self.model.causal_matrix).flatten())
-#         return np.stack(W_res_lst, axis=0)
-
-# model = NoTearsLinearCausalDiscoveryModel(10, 20, 100, 'linear', 'gauss')
-# model.set_parameters({"noise_scale": np.array([1]*100)})
-# results = model.simulate()
 
 # def simulate_equation(X, w, Z):
 #     return X @ w + Z
@@ -49,37 +28,41 @@ from castle.datasets import DAG, IIDSimulation
 
 # X = simulate_dag(W_true, Z)
 
-# print(X)
 
-if __name__ == '__main__':
+class NoTearsLinearCausalDiscoveryModel(pygpc.AbstractModel):
 
-    class NoTearsLinearCausalDiscoveryModel(pygpc.AbstractModel):
-
-        def __init__(self, N_nodes, N_edges, N_learn_samples, N_res_samples, method, sem_type):
-            self.N_nodes = N_nodes
-            self.N_edges = N_edges
-            self.N_learn_samples = N_learn_samples
-            self.N_res_samples = N_res_samples
-            self.method = method
-            self.sem_type = sem_type
-            self.model = Notears()
-        
-        def simulate(self, process_id=None, matlab_engine=None):
-            W_res_lst = []
+    def __init__(self, N_nodes, N_edges, N_learn_samples, N_res_samples, method, sem_type):
+        self.N_nodes = N_nodes
+        self.N_edges = N_edges
+        self.N_learn_samples = N_learn_samples
+        self.N_res_samples = N_res_samples
+        self.method = method
+        self.sem_type = sem_type
+        self.model = Notears()
+    
+    def simulate(self, process_id=None, matlab_engine=None):
+        W_true = DAG.scale_free(n_nodes=self.N_nodes, n_edges=self.N_edges)
+        W_result_lst = []
+        N_iterations = len(self.p["noise_scale"]) * len(range(self.N_res_samples))
+        with tqdm(total=N_iterations) as pbar:
             for noise_scale in self.p["noise_scale"]:
-                W_inner_res_lst = []
+                W_inner_result_lst = []
                 for _ in range(self.N_res_samples):
-                    W_true = DAG.scale_free(n_nodes=self.N_nodes, n_edges=self.N_edges)
                     data = IIDSimulation(W=W_true, n=self.N_learn_samples, method=self.method, sem_type=self.sem_type, noise_scale=float(noise_scale))
                     self.model.learn(data.X)
-                    W_inner_res_lst.append((W_true - self.model.causal_matrix).flatten())
-                W_res_lst.append(np.mean(np.stack(W_inner_res_lst, axis=0), axis=0))
-            return np.stack(W_res_lst, axis=0)
+                    W_inner_result_lst.append((W_true - self.model.causal_matrix).flatten())
+                    pbar.update(1)
+                W_result_lst.append(W_inner_result_lst)
+        return np.array(W_result_lst)
 
+
+if __name__ == '__main__':
+    
+    logging.disable(logging.CRITICAL)
 
     N_nodes = 10
     N_edges = 20
-    N_learn_samples = 100
+    N_learn_samples = 1000
     N_res_samples = 100
     method = "linear"
     sem_type = "gauss"
@@ -88,4 +71,6 @@ if __name__ == '__main__':
     model.set_parameters({"noise_scale": np.array([1, 10, 100])})
     results = model.simulate()
 
-    np.save("NoTearsResults.npy")
+    print(results)
+
+    np.save("NoTears_results.npy", results)
